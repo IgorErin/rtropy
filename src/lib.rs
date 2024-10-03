8d4e5205b8b04cfc220489b17750feae5ad52ec2
IgorErin <erin.igore@yandex.ru>
2024-10-03 10:39:12 +0300

    refactor: inline lib into main

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,4 @@
-use std::collections::HashMap;
-
-pub type Int = i32;
-pub type Float = f64;
-
-pub fn count(chars: impl Iterator<Item = char>) -> Vec<(char, Int)> {
-    let mut counts = HashMap::new();
-
-    for ch in chars {
-        *(counts.entry(ch).or_default()) += 1;
-    }
-
-    counts.into_iter().collect()
-}
-
-pub fn compute(counts: Vec<Int>) -> Float {
-    let mut result: Float = Default::default();
-    let nall: Int = counts.iter().sum();
-
-    for count in counts {
-        let prob = (count as Float) / (nall as Float);
-        result -= prob * Float::log2(prob);
-    }
-
-    result
-}
-
-pub fn fround(f: Float, num: u32) -> Float {
-    let m = 10i32.pow(num) as Float;
+pub fn fround(f: f32, num: u32) -> f32 {
+    let m = 10i32.pow(num) as f32;
     (f * m).round() / m
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
-use std::fs;
+use std::collections::HashMap;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -7,22 +11,41 @@ struct Args {
     spath: String,
 }
 
+const ROUND: u32 = 4;
+
 fn main() {
     let args = Args::parse();
-    let sfile = fs::read_to_string(args.spath);
+    let mut f = BufReader::new(File::open(args.spath).expect("open failed"));
 
-    let sfile = match sfile {
-        Err(err) => {
-            eprintln!("Cannot open file: {err}");
-            return;
+    type Int = i32;
+    let (counts, nall) = {
+        let mut counts: HashMap<char, Int> = HashMap::new();
+        let mut nall = 0;
+
+        let mut buf: Vec<u8> = Vec::<u8>::new();
+        while f.read_until(b'\n', &mut buf).expect("read_until failed") != 0 {
+            let s = String::from_utf8(buf).expect("from_utf8 failed");
+
+            for ch in s.chars() {
+                *(counts.entry(ch).or_default()) += 1;
+                nall += 1;
+            }
+
+            buf = s.into_bytes();
+            buf.clear();
         }
-        Ok(file) => file,
+        (counts, nall)
     };
 
-    let res = rtropy::count(sfile.chars());
-    let res = res.iter().map(|x| x.1);
-    let res = rtropy::compute(res.collect());
-    let res = rtropy::fround(res, 4);
+    type Float = f32;
+    let entropy = {
+        let mut result: Float = Default::default();
+        for (_, count) in counts.into_iter() {
+            let prob = (count as Float) / (nall as Float);
+            result -= prob * Float::log2(prob);
+        }
+        result
+    };
 
-    println!("entropy: {res}")
+    println!("entropy: {}", rtropy::fround(entropy, ROUND));
 }
