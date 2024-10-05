c20dd44864b46447219714d2c3a1438b4e3e462d
IgorErin <erin.igore@yandex.ru>
2024-10-05 10:28:31 +0300

    refactor: decouple count and compute with main

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,47 @@
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader, Read};
+
+pub type Int = i32;
+pub type Float = f32;
+
 pub fn fround(f: f32, num: u32) -> f32 {
     let m = 10i32.pow(num) as f32;
     (f * m).round() / m
+}
+
+pub fn count(f: impl Read, _: u32) -> std::io::Result<(Vec<Int>, Int)> {
+    let mut f = BufReader::new(f);
+
+    let mut counts: HashMap<u8, Int> = HashMap::new();
+    let mut nall = 0;
+    let mut readed = 0;
+
+    loop {
+        f.consume(readed);
+        let bytes = f.fill_buf()?;
+        readed = bytes.len();
+
+        if readed == 0 {
+            break;
+        }
+
+        for ch in bytes {
+            *(counts.entry(*ch).or_default()) += 1;
+            nall += 1
+        }
+    }
+
+    let counts = counts.values().cloned().collect();
+    Ok((counts, nall))
+}
+
+pub fn compute(counts: Vec<Int>, nall: Int) -> Float {
+    let mut result: Float = 0.0;
+
+    for count in counts.into_iter() {
+        let prob = (count as Float) / (nall as Float);
+        result -= prob * Float::log2(prob);
+    }
+
+    result
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use clap::Parser;
-use std::collections::HashMap;
-use std::{
-    fs::File,
-    io::{BufRead, BufReader},
-};
+use std::fs::File;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -13,43 +9,14 @@ struct Args {
 
 const ROUND: u32 = 4;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    let mut f = BufReader::new(File::open(args.spath).expect("open failed"));
+    let f = File::open(args.spath)?;
 
-    type Int = i32;
-    let (counts, nall) = {
-        let mut counts: HashMap<u8, Int> = HashMap::new();
-        let mut nall = 0;
-        let mut readed = 0;
-
-        loop {
-            f.consume(readed);
-            let bytes = f.fill_buf().expect("read failed");
-            readed = bytes.len();
-
-            if readed == 0 {
-                break;
-            }
-
-            for ch in bytes {
-                *(counts.entry(*ch).or_default()) += 1;
-                nall += 1
-            }
-        }
-
-        (counts, nall)
-    };
-
-    type Float = f32;
-    let entropy = {
-        let mut result: Float = Default::default();
-        for (_, count) in counts.into_iter() {
-            let prob = (count as Float) / (nall as Float);
-            result -= prob * Float::log2(prob);
-        }
-        result
-    };
+    let (counts, nall) = rtropy::count(f, Default::default())?;
+    let entropy = rtropy::compute(counts, nall);
 
     println!("entropy: {}", rtropy::fround(entropy, ROUND));
+
+    Ok(())
 }
